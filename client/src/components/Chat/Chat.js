88735365bc925d406fb855e5f242514fb4f2dd8c
DasithDeelaka <deelakajagoda@gmail.com>
2020-11-09 05:35:10 +0530

    Passing name & room as context rather than query string parameters

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,26 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import queryString from 'query-string';
+import React, { useState, useEffect, useContext } from 'react';
+// import queryString from 'query-string';
 import io from "socket.io-client";
 
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
 import TextContainer from '../TextContainer/TextContainer';
+import { UserContext } from '../../context';
 
 import './Chat.css';
 
 let socket;
 
 const Chat = ({ location }) => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
+    const [username, setName] = useState('');
+    const [chatRoom, setRoom] = useState('');
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const ENDPOINT = 'http://localhost:5000/';
 
+    const { name, room } = useContext(UserContext);
+
     useEffect(() => {
-        const { name, room } = queryString.parse(location.search);
+        // const { name, room } = queryString.parse(location.search);
 
         socket = io(ENDPOINT);
 
@@ -38,7 +41,7 @@ const Chat = ({ location }) => {
 
             socket.off();
         }
-    }, [ENDPOINT, location.search]);
+    }, [ENDPOINT, name, room]);
 
     useEffect(() => {
         socket.on('message', (message) => {
@@ -63,8 +66,8 @@ const Chat = ({ location }) => {
     return(
         <div className="outerContainer">
             <div className="container">
-                <InfoBar room={room} />
-                <Messages messages={messages} name={name} />
+                <InfoBar room={chatRoom} />
+                <Messages messages={messages} name={username} />
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
             <TextContainer users={users}/>
--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../context';
 
 import './Join.css';
 
 const Join = () => {
+
+    const { getDetails } = useContext(UserContext);
+
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
 
@@ -14,7 +18,7 @@ const Join = () => {
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
                 <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
                 <Link onClick={(event) => (!name || !room) ? event.preventDefault() : null} to={`chat?name=${name}&room=${room}`}>
-                    <button className="button mt-20" type="submit">Sign In</button>
+                    <button className="button mt-20" type="submit" onClick={() => getDetails(name,room)}>Sign In</button>
                 </Link>
             </div>
         </div>
