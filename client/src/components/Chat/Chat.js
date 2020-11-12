60237c80d8c7af248e6065ecbb8bb830e05f5648
DasithDeelaka <deelakajagoda@gmail.com>
2020-11-12 16:56:09 +0530

    Deploy frontend to the Netlify & backend to the Heroku

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'http://localhost:5000/';
+    const ENDPOINT = 'https://chatapp-react-node.herokuapp.com/';
 
     let history = useHistory();
 
